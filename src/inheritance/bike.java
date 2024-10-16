2cb3f574623dd95ca4605825f5a23ce154e5ca28
ramakrishnareddya <ramakrishna.aamuru@gmail.com>
2024-10-16 14:02:59 +0530

    java

---
+++ src/inheritance/bike.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class bike {
+	public static void main(String[] args) {
+
+		int a = 10;
+		System.out.println(a);
+	}
+
+
+	
+}
---
+++ src/inheritance/main.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class main {
+	public static void main(String[] args) {
+		
+	}
+
+}
